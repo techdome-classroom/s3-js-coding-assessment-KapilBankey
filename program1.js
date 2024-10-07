43164b65c1e55307bdf12e2569ed21372b6ce842
Kapil Bankey <kapilgurjar114@gmail.com>
2024-10-07 15:19:05 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -8,6 +8,13 @@ var isValid = function(s) {
       return false;
     }
     else { 
+      let c1=0;
+      let c2 =0;
+      let c3 = 0;
+      let c4 = 0;
+      let c5 = 0;
+      let c6 = 0;
+
        for(var i=0;i<len;i++){
         if(s[i] == "(" || s[i] ==")" || s[i] == "[" || s[i] == "]" || s[i] == "{" || s[i] == "}");
         c1++;
