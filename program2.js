d9c3ca7f6c6018158fd4b708f798bf7842dd34d0
Kapil Bankey <kapilgurjar114@gmail.com>
2024-10-07 15:29:06 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,10 +3,22 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+  const romanMap = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 };
+  let total = 0, prevValue = 0;
+  
+  for (let i = s.length - 1; i >= 0; i--) {
+      const currentValue = romanMap[s[i]];
+      total += currentValue < prevValue ? -currentValue : currentValue;
+      prevValue = currentValue;
+  }
+
+  return total;
 };
 
+const userInput = prompt("Enter a Roman numeral:");
+console.log(romanToInt(userInput));
 
-module.exports={romanToInt}
+module.exports = { romanToInt };
+
 
 
