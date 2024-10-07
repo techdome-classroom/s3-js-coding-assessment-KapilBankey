b7142fa4d55b73dcf82474c038a80488349c0b48
Kapil Bankey <kapilgurjar114@gmail.com>
2024-10-07 15:21:17 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,11 +1,23 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
 var isValid = function(s) {
-    
+  let stack = [];
+  
+  const matchingBrackets = {
+      ')': '(',
+      '}': '{',
+      ']': '['
+  };
+  
+  for (let char of s) {
+      if (char === '(' || char === '{' || char === '[') {
+          stack.push(char);
+      } else {
+          if (stack.length === 0 || stack.pop() !== matchingBrackets[char]) {
+              return false;
+          }
+      }
+  }
+  
+  return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
